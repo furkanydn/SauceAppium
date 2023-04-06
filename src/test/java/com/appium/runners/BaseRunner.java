61708bf3f7387e4403dfc559c818658e216ffb0f
furkanydn <f.aydin@outlook.com.tr>
2023-04-06 19:18:51 +0300

    BaseRunner.java completed.

--- src/test/java/com/appium/runners/BaseRunner.java
+++ src/test/java/com/appium/runners/BaseRunner.java
@@ -4,12 +4,14 @@ package com.appium.runners;
 import com.appium.manager.DriverManager;
 import com.appium.manager.GlobalParameters;
 import com.appium.manager.ServerManager;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
 import org.apache.logging.log4j.ThreadContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-
-import java.sql.Driver;
+import org.testng.annotations.Test;
 
 public class BaseRunner {
     private static final ThreadLocal<TestNGCucumberRunner> testCucumberRunner = new ThreadLocal<>();
@@ -22,10 +24,26 @@ public class BaseRunner {
         testCucumberRunner.set(cucumberRunner);
     }
 
+    @DataProvider
+    public static Object[][] scenarios() {
+        return getCucumberRunner().provideScenarios();
+    }
+
+    /**
+     * It initializes the Appium service and driver by calling the necessary methods inside.
+     * @param platformName e.g. iOS
+     * @param udid e.g. 1ae203187fc012g
+     * @param deviceName e.g. iPhone12
+     * @param systemPort e.g. 10000
+     * @param driverPort e.g. 11000
+     * @param wdaPort e.g. 10001
+     * @param webkitProxyPort e.g. 11001
+     * */
+    @BeforeClass(alwaysRun = true)
     public void setupClass(String platformName, String udid, String deviceName,
                            @Optional("Android") String systemPort, @Optional("Android") String driverPort,
                            @Optional("iOS") String wdaPort, @Optional("iOS") String webkitProxyPort)
-            throws Exception{
+            throws Exception {
         ThreadContext.put("ROUTINGKEY", platformName + "_" + deviceName);
         GlobalParameters globalParameters = new GlobalParameters();
         globalParameters.setPlatformName(platformName);
@@ -45,5 +63,32 @@ public class BaseRunner {
         }
         new ServerManager().startServiceLocal();
         new DriverManager().initDriverThread();
+        setCucumberRunner(new TestNGCucumberRunner(this.getClass()));
+    }
+
+    /**
+     * Cucumber puts scenarios in them.
+     * @param pickleWrapper
+     * @param featureWrapper
+     * */
+    @Test(groups = "cucumber",description = "Runs Cucumber scenarios",dataProvider = "scenarios")
+    public void scenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        getCucumberRunner().runScenario(pickleWrapper.getPickle());
+    }
+
+    /**
+     * Stops this service is it is currently running. This method will attempt to block until the
+     * server has been fully shutdown.
+     */
+    public void tearDown(){
+        DriverManager driverManager = new DriverManager();
+        ServerManager serverManager = new ServerManager();
+
+        if (driverManager.getDriverThreadLocal() != null){
+            driverManager.getDriverThreadLocal().quit();
+            driverManager.setDriverThreadLocal(null);
+        }
+        if (serverManager.getAppiumService() != null) serverManager.getAppiumService().stop();
+        getCucumberRunner().finish();
     }
 }
