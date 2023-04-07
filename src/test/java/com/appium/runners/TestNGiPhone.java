91b6ea407ff6ca53bfc26a758de7af6470ef779f
furkanydn <f.aydin@outlook.com.tr>
2023-04-07 17:27:16 +0300

    TestNGiPhone cucumber report completed.

---
+++ src/test/java/com/appium/runners/TestNGiPhone.java
@@ -0,0 +1,15 @@
+package com.appium.runners;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {
+                "pretty","html:target_TestNG/iPhone/report.html",
+                "summary",
+                "me.jvt.cucumber.report.PrettyReports:target_TestNG/iPhone/" },
+        features = {"src/test/resources/features"},
+        glue = {"com.appium.stepDefinitions"},
+        monochrome = true,
+        tags = "@login-feature")
+public class TestNGiPhone extends BaseRunner{
+}
