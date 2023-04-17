9b2b087f48b758e8924d69c1ab7e76013ddea034
furkanydn <f.aydin@outlook.com.tr>
2023-04-17 18:16:47 +0300

    base methods added.

--- src/main/java/com/appium/pages/Base.java
+++ src/main/java/com/appium/pages/Base.java
@@ -5,11 +5,13 @@ import com.appium.utils.TestUtilities;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 public class Base {
     protected static AppiumDriver baseDriver;
@@ -34,10 +36,57 @@ public class Base {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
-    public void click(AppiumBy by){
+    /**
+     * Click this element. If this causes a new page to load, you
+     * should discard all references to this element and any further
+     * operations performed on this element will throw a
+     * StaleElementReferenceException.
+     *
+     * @throws StaleElementReferenceException If the element no
+     *     longer exists as initially defined
+     */
+    public void click(AppiumBy by) {
         waitForVisible(by);
-        utilities.logger().info("Clicked by " + by);
-        //Temporary
-        baseDriver.findElement(AppiumBy.ByAccessibilityId(by)).click();
+        try {
+            utilities.logger().info("Clicked by " + by);
+            //Temporary
+            baseDriver.findElement(by).click();
+        } catch (StaleElementReferenceException exception) {
+            exception.printStackTrace();
+        }
+    }
+    /**
+     * Use this method to simulate typing into an element, which may set its value.
+     *
+     * @param keysToSend character sequence to send to the element
+     *
+     * @throws IllegalArgumentException if keysToSend is null
+     */
+    public void sendKeys(AppiumBy by, String keysToSend){
+        waitForVisible(by);
+        try {
+            utilities.logger().info("Element is %s and keys to %s".formatted(by, keysToSend));
+            //Temporary
+            baseDriver.findElement(by).sendKeys(keysToSend);
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
+    }
+    /**
+     * Get the visible (i.e. not hidden by CSS) text of this element, including sub-elements.
+     *
+     * @return The visible text of this element.
+     */
+    public String getKeys(AppiumBy by){
+        waitForVisible(by);
+        String text = null;
+        try {
+            //Temporary
+            text = baseDriver.findElement(by).getText();
+            utilities.logger().info("Element is %s and getting keys to %s".formatted(by, text));
+        } catch (NoSuchElementException exception) {
+            exception.printStackTrace();
+        }
+        return text;
     }
 }
