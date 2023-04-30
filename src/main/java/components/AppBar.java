b5cc8dfb5b4334d3a9b8368203b44c2165bbcd1e
furkanydn <f.aydin@outlook.com.tr>
2023-04-30 23:17:01 +0300

    AppBar.java some javadoc added.

--- src/main/java/components/AppBar.java
+++ src/main/java/components/AppBar.java
@@ -8,6 +8,10 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ The AppBar class represents the navigation bar that is present in most mobile applications.
+ It extends the BasePage class and inherits its methods.
+ */
 public class AppBar extends BasePage {
     /**
      * Clicks on the "Open Menu" button to reveal the menu options.
@@ -15,24 +19,38 @@ public class AppBar extends BasePage {
     public void openMenu() {
         findElement("open menu").click();
     }
-
+    /**
+     Scrolls horizontally to hide the Android menu.
+     */
     public void closeAndroidMenu(){
         horizontalScroll("android:id/content");
     }
-
+    /**
+     Clicks on the "Sort" button and selects the given sorting option.
+     @param sortComp the sorting option to select.
+     */
     public void sortButton(SortComp sortComp){
         findElement("sort button").click();
         //findElement(getSortingName(sortComp)).click();
     }
-
+    /**
+     Clicks on the cart badge icon.
+     */
     public void cartBadge(){
         findElement("cart badge").click();
     }
-
+    /**
+     An enum that represents the available sorting options.
+     */
     public enum SortComp {
         NAME_ASC, NAME_DESC, PRICE_ASC, PRICE_DESC
     }
-
+    /**
+     Returns the name of the sorting option as a string.
+     @param sortComp the sorting option to get the name for.
+     @return the name of the sorting option as a string.
+     @throws IllegalArgumentException if an invalid sorting option is provided.
+     */
     public static String getSortingName(SortComp sortComp) {
         switch (sortComp) {
             case NAME_ASC -> {
@@ -50,7 +68,10 @@ public class AppBar extends BasePage {
             default -> throw new IllegalArgumentException("Invalid sorting name " + sortComp);
         }
     }
-
+    /**
+     Scrolls horizontally on the screen.
+     @param contentId the id of the element to scroll.
+     */
     public void horizontalScroll(String contentId){
         int centerY = findElementId(contentId).getRect().y + (findElementId(contentId).getSize().height/2);
         double startX = findElementId(contentId).getRect().x + (findElementId(contentId).getSize().width * 0.8);
