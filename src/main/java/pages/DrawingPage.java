081725bc694fe8083471dfddbf36673fc63bc57f
furkanydn <f.aydin@outlook.com.tr>
2023-06-19 12:10:05 +0300

    Face drawing made according to device dimension

--- src/main/java/pages/DrawingPage.java
+++ src/main/java/pages/DrawingPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import components.MenuItem;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import touch.Drawer;
 import utils.Config;
@@ -10,25 +11,36 @@ public class DrawingPage extends BasePage {
     public void goDrawingPageWithClicks(){
         new MenuItem().Drawing();
     }
-    public void drawFace(){
-        Point head = new Point(220,450);
-        Point leftEye = head.moveBy(-50, -50);
-        Point rightEye = head.moveBy(50, -50);
-        Point mouth = head.moveBy(0, 50);
+    public void drawFace() {
+        try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+        Point head = new Point(getDimension(Dimensions.WIDTH) / 2, getDimension(Dimensions.HEIGHT) / 2);
+        Point leftEye = head.moveBy(-getDimension(Dimensions.WIDTH) / 10, -getDimension(Dimensions.HEIGHT) / 10);
+        Point rightEye = head.moveBy(getDimension(Dimensions.WIDTH) / 10, -getDimension(Dimensions.HEIGHT) / 10);
+        Point mouth = head.moveBy(0, getDimension(Dimensions.HEIGHT) / 10);
 
-        switch (Config.platformName){
+        switch (Config.platformName) {
             case ANDROID -> {
-                new Drawer().drawCircle(androidDriver,head,120,30);
-                new Drawer().drawCircle(androidDriver,leftEye, 20, 20);
-                new Drawer().drawCircle(androidDriver, rightEye, 20, 20);
-                new Drawer().drawCircle(androidDriver, mouth, 40, 20);
+                new Drawer().drawCircle(androidDriver, head, (double) getDimension(Dimensions.HEIGHT) / 6, 50);
+                new Drawer().drawCircle(androidDriver, leftEye, (double) getDimension(Dimensions.HEIGHT) / 30, 30);
+                new Drawer().drawCircle(androidDriver, rightEye, (double) getDimension(Dimensions.HEIGHT) / 30, 30);
+                new Drawer().drawCircle(androidDriver, mouth, (double) getDimension(Dimensions.HEIGHT) / 20, 40);
             }
             case IOS -> {
-                new Drawer().drawCircle(iosDriver,head,120,30);
-                new Drawer().drawCircle(iosDriver,leftEye, 20, 20);
-                new Drawer().drawCircle(iosDriver, rightEye, 20, 20);
-                new Drawer().drawCircle(iosDriver, mouth, 40, 20);
+                new Drawer().drawCircle(iosDriver, head, (double) getDimension(Dimensions.HEIGHT) / 6, 50);
+                new Drawer().drawCircle(iosDriver, leftEye, (double) getDimension(Dimensions.HEIGHT) / 30, 30);
+                new Drawer().drawCircle(iosDriver, rightEye, (double) getDimension(Dimensions.HEIGHT) / 30, 30);
+                new Drawer().drawCircle(iosDriver, mouth, (double) getDimension(Dimensions.HEIGHT) / 20, 40);
             }
         }
+    }
+    public enum Dimensions {WIDTH,HEIGHT}
+
+    private int getDimension(Dimensions dimension) {
+        Dimension size = switch (Config.platformName) {
+            case ANDROID -> androidDriver.manage().window().getSize();
+            case IOS -> iosDriver.manage().window().getSize();
+        };
+
+        return dimension == Dimensions.WIDTH ? size.getWidth() : size.getHeight();
     }
 }
